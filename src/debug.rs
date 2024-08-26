42ba2dec0fbd3bd1dcffc622f3cf05004ba650f0
Supermjork <markehabaziz@gmail.com>
2024-08-26 16:29:07 +0300

    Modularity over files

---
+++ src/debug.rs
@@ -0,0 +1,16 @@
+use bevy::prelude::*;
+
+pub struct DebugPlugin;
+
+impl Plugin for DebugPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, print_position);
+    }
+}
+
+fn print_position(query: Query<(Entity, &Transform)>) {
+    // Logging ID + Position for each entity
+    for (entity, transform) in query.iter() {
+        info!("Entity {:?} is at Position ({:?}).", entity, transform.translation);
+    }
+}
