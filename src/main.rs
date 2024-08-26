3cd4b5770049d946443abb2827d03a292023fc2c
Supermjork <markehabaziz@gmail.com>
2024-08-26 16:46:14 +0300

    Added camera + lighting

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 mod debug;
 mod movement;
 mod spaceship;
+mod camera;
 
 use bevy::prelude::*;
 
 use debug::DebugPlugin;
 use movement::MovementPlugin;
 use spaceship::SpaceshipPlugin;
+use camera::CameraPlugin;
 
 fn main() {
     App::new()
+        // Bevy Built-ins
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.75,
+        })
         .add_plugins(DefaultPlugins)
         // User Defined Plugins
         .add_plugins(SpaceshipPlugin)
+        .add_plugins(CameraPlugin)
         .add_plugins(MovementPlugin)
         .add_plugins(DebugPlugin)
         .run();
